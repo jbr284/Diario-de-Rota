a7a3ea3877f37cbc87b3f9baa9f10d5afe5c9bff
jbr284 <jbrosa2009@gmail.com>
2026-02-23 23:32:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Mudamos para v2 para forçar a atualização no telemóvel dos utilizadores
+// Mudamos para v3 para forçar a atualização no telemóvel dos utilizadores
 const CACHE_NAME = "diario-de-rota-v2";
 
 const ASSETS_TO_CACHE = [
